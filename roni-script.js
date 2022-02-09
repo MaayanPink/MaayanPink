d4047f0d39993e160790dcad0495e805f08aa3a7
MaayanPink <94897069+MaayanPink@users.noreply.github.com>
2022-02-09 11:12:08 +0200

    changed name

--- roni-script.js
+++ roni-script.js
@@ -36,44 +36,6 @@ $(document).ready(function() {
     }, function() {
         $(this).css('color', 'black').css('background-color', '#f9f5f1');
     });
-
-
-    'use strict';
-
-    $(function() {
-
-        //settings for slider
-        var width = 720;
-        var animationSpeed = 500;
-        var pause = 2500;
-        var currentSlide = 1;
-
-        //cache DOM elements
-        var $slider = $('#slider');
-        var $slideContainer = $('.slides', $slider);
-        var $slides = $('.slide', $slider);
-        var interval;
-
-        function startSlider() {
-            interval = setInterval(function() {
-                $slideContainer.animate({
-                    'margin-left': '-=' + width
-                }, animationSpeed, function() {
-                    if (++currentSlide === $slides.length) {
-                        currentSlide = 1;
-                        $slideContainer.css('margin-left', 0);
-                    }
-                });
-            }, pause);
-        }
-
-        function pauseSlider() {
-            clearInterval(interval);
-        }
-        $slideContainer.on('mouseenter', pauseSlider).on('mouseleave', startSlider);
-        startSlider();
-    });
-
     $(".so-img").hover(function() {
         $(this).animate({ opacity: '1' }, 0);
     }, function() {
