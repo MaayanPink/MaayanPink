8f5134b3c10b4189ca3e32db95061d6e74bae3c1
MaayanPink <94897069+MaayanPink@users.noreply.github.com>
2022-02-20 09:40:39 +0200

    fixed js

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,35 +1,28 @@
-$(document).ready(function() {
+function show1() {
+    const loginContainer1 = document.getElementById('login-container1');
+    const loginContainer2 = document.getElementById('login-container2');
+    loginContainer2.style.display = "none";
+    if (loginContainer1.style.display === "none") {
+        loginContainer1.style.display = "inline-block";
+    } else {
+        loginContainer1.style.display = "none";
+    }
+};
 
-    $('.login-container1').hide();
-    $('#sign-up').click(function() {
-        $('.login-container2').hide();
-        $(".login-container1").toggle(1000);
+function show2() {
+    const loginContainer1 = document.getElementById('login-container1');
+    const loginContainer2 = document.getElementById('login-container2');
+    loginContainer1.style.display = "none";
+    if (loginContainer2.style.display === "none") {
+        loginContainer2.style.display = "inline-block";
+    } else {
+        loginContainer2.style.display = "none";
+    }
+};
 
-    });
-
-    $('.login-container2').hide();
-    $('#login').click(function() {
-        $('.login-container1').hide();
-        $(".login-container2").toggle(1000);
-    });
-    document.getElementById('signup-email').value = ''
-    document.getElementById('signup-password').value = ''
-    $('a, #sign-up, #login, #logout').hover(function() {
-            $(this).css('color', '#0dd0d3');
-        },
-        function() {
-            $(this).css('color', '#065e60');
-        });
-    $('.add-to-cart').hover(function() {
-        $(this).css('color', '#c9ada7').css('background-color', '#fff');
-    }, function() {
-        $(this).css('color', '#fff').css('background-color', '#c9ada7');
-    });
-    var count = 0;
-    $('.add-to-cart').click(function() {
-        count++;
-        $('.cart_count').html(count)
-    });
-
-
+//cart counter ++ 
+var count = 0;
+$('.add-to-cart').click(function() {
+    count++;
+    $('.cart_count').html(count)
 });
